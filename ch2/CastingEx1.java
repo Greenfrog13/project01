9308d0935aaa1197b67c44e45282ce9c7f17c43a
JiwonJeong <50576758+Greenfrog13@users.noreply.github.com>
2021-05-19 01:08:34 +0900

    예제 2-12
    형변환 방법

--- ch2/CastingEx1.java
+++ ch2/CastingEx1.java
@@ -1,1 +1,9 @@
-class 
+class CastingEx1 {
+  public static void main(String[] args){
+    double d = 85.4;
+    int score = (int)d;
+    
+    System.out.println("score"+score);
+    System.out.println("d="+d);
+  }
+}
