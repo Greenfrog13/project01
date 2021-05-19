c671abc889ca374b7c45544d60a781e36f94c3d3
JiwonJeong <50576758+Greenfrog13@users.noreply.github.com>
2021-05-19 16:42:28 +0900

    예제 3-5
    사칙 연산자 + - * /

---
+++ ch3/OperatorEx5.java
@@ -0,0 +1,13 @@
+class OperatorEx5 {
+	public static void main(String[] args){
+		int a = 10;
+		int b = 4;
+		
+		System.out.println("%d + %d = %d\n", a, b, a+b);
+		System.out.println("%d - %d = %d\n", a, b, a-b);
+		System.out.println("%d * %d = %d\n", a, b, a*b);
+		System.out.println("%d / %d = %d\n", a, b, a/b);
+		System.out.println("%d / %f = %d\n", a, (float)b, a/(float)b);
+	}
+}
+		
